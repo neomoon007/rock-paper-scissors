147b0776b67d66e5e42b519585d6088184df2c59
Marcos R <costamarcos404@gmail.com>
2022-02-19 18:00:47 -0300

    html file and pseudocode script file done

---
+++ script.js
@@ -0,0 +1,52 @@
+// Create a function called computerPlay that returns the computer choice
+// Create a variable called choice that stores the randomly selected value between ("rock","paper","scissors")
+// Return $choice
+// End computerPlay function
+
+// Create a function called playRound that receive (playerSelection, computerSelection) and returns the winner on this round
+// Create a variable called playerChoice that stores the playerSelection with Uppercase on the first letter and lowercase on the rest
+// Create a variable called computerChoice that stores the computerSelection with Uppercase on the first letter and lowercase on the rest
+
+// Create a variable called playerWin1 that stores playerChoice === "Rock" && computerChoice === "Scissors"
+// Create a variable called playerWin2 that stores playerChoice === "Paper" && computerChoice === "Rock"
+// Create a variable called playerWin3 that stores playerChoice === "Scissors" && computerChoice === "Paper"
+ 
+// If ( playerChoice == computerChoice )
+//      Print "It's a tie!"
+//      Return "tie"
+// ElseIf ( playerWin1 || playerWin2 || playerWin3 )
+//      Print "You Win! $playerChoice beats $computerChoice"
+//      Return "player"
+// Else
+//      Print "You Lose! $computerChoice beats $playerChoice"
+//      Return "computer"
+// EndIf
+// End playRound function
+
+// Create a function called game that runs the game and return the winner and the final score
+// Create a variable called playerScore
+// Create a variable called computerScore
+// Create a variable called playerMove
+// Create a variable called roundResult 
+
+// Create a Loop that run 5 times
+// playerMove receive player input
+// roundResult receive the playRound(playerMove, computerPlay()) function returned value
+// If ( roundResult == "player" )
+//     increment playerScore
+// ElseIf ( roundResult == "computer" )
+//     increment computerScore
+// EndIf
+// EndLoop
+ 
+// If ( playerScore > computerScore )
+//     Print "You Won the game!"
+// Elseif ( computerScore > playerScore )
+//     Print "You Lost the game!"
+// Elseif ( playerScore == computerScore )
+//     Print "It's a tie!"
+// Endif
+
+// Print "You: $playerScore"
+// Print "Computer: $computerScore" 
+// End game function
